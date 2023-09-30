b240608a8ace1499711c596fd0fb5b34634da869
I-M-Marinov <lcfrrr@gmail.com>
2023-09-30 13:57:53 +0300

    Add project files.

---
+++ CarManufacturer/Car.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private int horsePower;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private int engineIndex;
+        private int tiresIndex;
+        private double totalPressure;
+
+
+        public Car(string make, string model, int year, int horsePower, double fuelQuantity,
+            double fuelConsumption, int engineIndex, int tiresIndex, double totalPressure)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            HorsePower = horsePower;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+            EngineIndex = engineIndex;
+            TiresIndex = tiresIndex;
+            TotalPressure = totalPressure;
+        }
+
+
+        public string Make
+        {
+            get { return make;}
+            set { make = value;}
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+        public int EngineIndex
+        {
+            get { return engineIndex; }
+            set { engineIndex = value; }
+        }
+
+        public int TiresIndex
+        {
+            get { return tiresIndex; }
+            set { tiresIndex = value; }
+        }
+        
+        public double TotalPressure
+        {
+            get { return totalPressure; }
+            set { totalPressure = value; }
+        }
+
+        public double DriveTwentyKm(double fuelQuantity, double fuelConsumption)
+        {
+            fuelQuantity -= (FuelConsumption / 100) * 20;
+
+            return fuelQuantity;
+        }
+    }
+
+}
+
+public class Engine
+{
+    private int horsePower;
+
+    public int HorsePower
+    {
+        get { return horsePower; }
+        set { horsePower = value; }
+    }
+
+
+    private double cubicCapacity;
+
+    public double CubicCapacity
+    {
+        get { return cubicCapacity; }
+        set { cubicCapacity = value; }
+    }
+
+    public Engine(int horsePower, double cubicCapacity)
+    {
+        HorsePower = horsePower;
+        CubicCapacity = cubicCapacity;
+    }
+
+    public int GetHorsePower(string[] splitFirstInput)
+    {
+        int horsePower = int.Parse(splitFirstInput[0]);
+
+        return horsePower;
+    }
+
+    public double GetCubicCapacity(string[] splitFirstInput)
+    {
+        double cubicCapacity = double.Parse(splitFirstInput[1]);
+
+        return cubicCapacity;
+    }
+}
+
+public class Tire
+{
+    private int year;
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    private double pressure;
+
+    public double Pressure
+    {
+        get { return pressure; }
+        set { pressure = value; }
+    }
+
+    public Tire(int year, double pressure)
+    {
+        Year = year;
+        Pressure = pressure;
+    }
+
+    public List<double> GetYearInfo(string[] splitSecondInput)
+    {
+        List<double> yearsList = new List<double>();
+
+        for (int i = 0; i < splitSecondInput.Length; i += 2)
+        {
+            yearsList.Add(int.Parse(splitSecondInput[i]));
+        }
+
+        return yearsList;
+    }
+
+    public List<double> GetPressureInfo(string[] splitSecondInput)
+    {
+        List<double> pressureList = new List<double>();
+
+        for (int i = 1; i < splitSecondInput.Length; i += 2)
+        {
+            pressureList.Add(double.Parse(splitSecondInput[i]));
+        }
+
+        return pressureList;
+    }
+
+    public double GetSumPressure(List<List<double>> tirePressureList,
+        int tires)
+    {
+        double sumPressure = tirePressureList[tires].Sum();
+
+        return sumPressure;
+    }
+}
---
+++ CarManufacturer/StartUp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<List<double>> listTiresYears = new List<List<double>>();
+            List<List<double>> listTiresPressures = new List<List<double>>();
+            List<int> listHorsePowers = new List<int>();
+            List<double> listCubicCapacity = new List<double>();
+
+            List<Car> listCars = new List<Car>();
+
+
+            string firstInput = Console.ReadLine();
+
+            Tire tires = new Tire(0,0);
+            Engine engine = new Engine(0,0);
+
+            while (firstInput != "No more tires")
+            {
+                string[] splitFirstInput = firstInput.Split();
+
+                List<double> yearsList = tires.GetYearInfo(splitFirstInput);
+                List<double> pressureList = tires.GetPressureInfo(splitFirstInput);
+
+                listTiresYears.Add(yearsList);
+                listTiresPressures.Add(pressureList);
+
+                firstInput = Console.ReadLine();
+            }
+
+            string secondInput = Console.ReadLine();
+
+            while (secondInput != "Engines done")
+            {
+                string[] splitSecondInput = secondInput.Split();
+
+                listHorsePowers.Add(engine.GetHorsePower(splitSecondInput));
+                listCubicCapacity.Add(engine.GetCubicCapacity(splitSecondInput));
+
+                secondInput = Console.ReadLine();
+            }
+
+            string thirdInput = Console.ReadLine();
+
+            while (thirdInput != "Show special")
+            {
+                string[] splitThirdInput = thirdInput.Split();
+
+                string make = splitThirdInput[0];
+                string model = splitThirdInput[1];
+                int year = int.Parse(splitThirdInput[2]);
+                double fuelQuantity = double.Parse(splitThirdInput[3]);
+                double fuelConsumption = double.Parse(splitThirdInput[4]);
+                int engineIndex = int.Parse(splitThirdInput[5]);
+                int tiresIndex = int.Parse(splitThirdInput[6]);
+
+                int horsePower = listHorsePowers[engineIndex];
+                double pressure = tires.GetSumPressure(listTiresPressures, tiresIndex);
+
+                Car car = new Car(make, model, year, horsePower, fuelQuantity, fuelConsumption,
+                    engineIndex, tiresIndex, pressure);
+
+                listCars.Add(car);
+
+
+                thirdInput = Console.ReadLine();
+            }
+
+            foreach (var car in listCars)
+            {
+                if (car.Year >= 2017 && car.HorsePower > 330
+                    && car.TotalPressure > 9 && car.TotalPressure < 10)
+                {
+                    car.FuelQuantity = car.DriveTwentyKm(car.FuelQuantity, car.FuelConsumption);
+
+                    Console.WriteLine($"Make: {car.Make}");
+                    Console.WriteLine($"Model: {car.Model}");
+                    Console.WriteLine($"Year: {car.Year}");
+                    Console.WriteLine($"HorsePowers: {car.HorsePower}");
+                    Console.WriteLine($"FuelQuantity: {car.FuelQuantity}");
+                }
+            }
+        }
+    }
+}
+
+    
+
+
+
